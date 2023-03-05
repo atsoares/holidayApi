c398d6c8b4c344e4ddca54d0f52df9ea96d5df10
Alexander Soares <alexander.soares@dxc.com>
2023-03-05 12:56:05 +0100

    first commit

---
+++ src/main/java/com/bluestone/pim/test/holidayapi/HolidayapiApplication.java
@@ -0,0 +1,13 @@
+package com.bluestone.pim.test.holidayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluestone/pim/test/holidayapi/HolidayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluestone.pim.test.holidayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
