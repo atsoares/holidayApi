707a59ac2538ea344e482e5c7aa14e7e1ebe7877
Alexander Soares <alexander.soares@dxc.com>
2023-03-09 17:43:53 +0100

    Swagger Config

---
+++ src/main/java/com/bluestone/pim/test/holidayapi/config/SwaggerConfig.java
@@ -0,0 +1,51 @@
+package com.bluestone.pim.test.holidayapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value("localhost:8080")
+    private String baseUrl;
+
+    @Value("Holiday API for TechInterview - Bluestone PIM")
+    private String apiTitle;
+
+    @Value("Holiday API")
+    private String apiInfo;
+
+    @Bean
+    public Docket microServicesApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .host(baseUrl)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build()
+                .useDefaultResponseMessages(false)
+                .apiInfo(metaData(apiInfo));
+    }
+
+    private ApiInfo metaData(String description) {
+        final String projectVersion = "1.0";
+        return new ApiInfoBuilder()
+                .description(description)
+                .version(projectVersion)
+                .title(apiTitle)
+                .contact(
+                        new Contact("techinterview-ia holidayApi", "https://github.com/atsoares", "aleexsoarees@gmail.com"))
+                .license("Holiday Api Test © 2023")
+                .build();
+    }
+
+}
---
+++ src/main/java/com/bluestone/pim/test/holidayapi/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.bluestone.pim.test.holidayapi.constants;
+
+public interface Constants {
+
+    String STATUS_200_GET_OK = "Successfully retrieved";
+    String STATUS_400_BAD_REQUEST = "Resource is invalid";
+    String STATUS_404_NOT_FOUND = "Resource not found";
+
+}
