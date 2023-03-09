78ebf3adeafa95a61ae076c3f7e80b3725bccf8c
Alexander Soares <alexander.soares@dxc.com>
2023-03-09 17:41:57 +0100

    POJO object response from holidayAPI

---
+++ src/main/java/com/bluestone/pim/test/holidayapi/model/HolidayResponse.java
@@ -0,0 +1,20 @@
+package com.bluestone.pim.test.holidayapi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class HolidayResponse {
+    @JsonProperty("date")
+    private LocalDate date;
+
+    @JsonProperty("name1")
+    private String name1;
+
+    @JsonProperty("name2")
+    private String name2;
+}
